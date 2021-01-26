5c3a7da946a83198b5aa6f8f48e2bf33bd44548e
git config --global user.name jeong-subingit config --global user.email subin930507@gmail.com <subin930507@gmail.com>
2021-01-26 19:01:31 +0900

    3번째 commit

--- src/com/board/controller/ActionFactory.java
+++ src/com/board/controller/ActionFactory.java
@@ -1,7 +1,12 @@
 package com.board.controller;
 
 import com.board.controller.action.Action;
+import com.board.controller.action.BoardCheckPassAction;
+import com.board.controller.action.BoardCheckPassFormAction;
+import com.board.controller.action.BoardDeleteAction;
 import com.board.controller.action.BoardListAction;
+import com.board.controller.action.BoardUpdateAction;
+import com.board.controller.action.BoardUpdateFormAction;
 import com.board.controller.action.BoardViewAction;
 import com.board.controller.action.BoardWriteAction;
 import com.board.controller.action.BoardWriteFormAction;
@@ -29,7 +34,17 @@ public class ActionFactory {
 			action = new BoardWriteAction();
 		} else if (command.equals("board_view")) {
 			action = new BoardViewAction();
-		} 
+		}  else if (command.equals("board_check_pass")) {
+			action = new BoardCheckPassAction();
+		}else if (command.equals("board_check_pass_form")) {
+			action = new BoardCheckPassFormAction();
+		}else if (command.equals("board_update_form")) {
+			action = new BoardUpdateFormAction();
+		}else if (command.equals("board_update")) {
+			action = new BoardUpdateAction();
+		}else if (command.equals("board_delete")) {
+			action = new BoardDeleteAction();
+		}         
 		return action;
 	}
 }
